e953228e9bc89960eb396db9a0dc6ae37bde1dff
Anne-Sophie Denommé-Pichon <anne-sophie.denomme-pichon@u-bourgogne.fr>
2019-11-18 10:33:10 +0100

    Create wrapper for sort bam per read

---
+++ wrapper_samtoolssortbyread.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+samtools sort -n -@64 -T /work/gad/shared/analyse/STR/Data/dijen017/lobSTR/2019-11-18  -o /work/gad/shared/analyse/STR/Data/dijen017/lobSTR/2019-11-18/dijen017.sorted.bam /work/gad/shared/analyse/STR/Data/dijen017/lobSTR/dijen017.bam
