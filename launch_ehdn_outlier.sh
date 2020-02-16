c7b5da6e9ec2f0af16f8ed31789bbc588308e691
Anne-Sophie Denommé-Pichon <anne-sophie.denomme-pichon@u-bourgogne.fr>
2020-02-16 14:59:22 +0100

    Handle ehdn outlier analyses on SGE for all patients

--- launch_ehdn_outlier.sh
+++ launch_ehdn_outlier.sh
@@ -13,10 +13,10 @@
 ## Last revision date: 20200215
 ## Known bugs: None
 
-CASE=dijen001
+CASE="$1"
 DATE="$(date +"%F_%H-%M-%S")"
 OUTPUTDIR="/work/gad/shared/analyse/STR/ExpansionHunterDeNovoOutlier/$DATE"
-LOGFILE="$OUTPUTDIR/$DATE.log"
+LOGFILE="$OUTPUTDIR/$DATE.$CASE.log"
 
 # Launch the script on local host with --local option and on SGE with qsub without the --local option
 if [ $# -eq 1 ] && [ "x$1" = x--local ]
@@ -25,5 +25,5 @@ then
     CASE="$CASE" LOGFILE="$LOGFILE" "$(dirname "$0")/wrapper_ehdn_outlier.sh"
 else 
     mkdir -p "$OUTPUTDIR"
-    qsub -pe smp 1 -q batch -v CASE="$CASE",LOGFILE="$LOGFILE" wrapper_ehdn_outlier.sh
+    qsub -pe smp 1 -q batch -N "ehdn_outlier_$CASE" -sync y -v CASE="$CASE",LOGFILE="$LOGFILE" "$(dirname "$0")/wrapper_ehdn_outlier.sh"
 fi
