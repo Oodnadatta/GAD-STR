6cccde098ac4e9c60fe0ce86b4d0afeb5682085e
Anne-Sophie Denommé-Pichon <anne-sophie.denomme-pichon@u-bourgogne.fr>
2020-06-07 00:51:13 +0200

    Remove scripts for HipSTR STRetch STRscan LobSTR

--- wrapper_samtoolssortbyread.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-samtools sort -n -@64 --check=quiet -T sortbyread  /work/gad/shared/analyse/STR/Data/dijen017/lobSTR/2019-11-26  -o /work/gad/shared/analyse/STR/Data/dijen017/lobSTR/2019-11-26/dijen017.sorted.bam /work/gad/shared/analyse/STR/Data/dijen017/lobSTR/dijen017.bam
